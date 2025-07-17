abfafecc98fe8098d40b74be313045cd77e8d7f6
GabrielSilveerio <silverio.gabriel2004@gmail.com>
2025-07-17 10:47:45 -0300

    Transacao Atualizado

--- src/main/java/com/backend/itau/mapper/TransacaoMapper.java
+++ src/main/java/com/backend/itau/mapper/TransacaoMapper.java
@@ -2,19 +2,20 @@ package com.backend.itau.mapper;
 
 import com.backend.itau.dto.TransacaoDTO;
 import com.backend.itau.model.TransacaoModel;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class TransacaoMapper {
 
     private final TransacaoDTO transacaoDTO;
     private final TransacaoModel transacaoModel;
 
-
+    @Autowired
     public TransacaoMapper(TransacaoDTO transacaoDTO, TransacaoModel transacaoModel) {
         this.transacaoDTO = transacaoDTO;
         this.transacaoModel = transacaoModel;
     }
 
-    public TransacaoDTO getTransacaoDTO(TransacaoModel transacaoModel) {
+    public TransacaoDTO toDTO(TransacaoModel transacaoModel) {
         TransacaoDTO transacaoDTO = new TransacaoDTO();
         transacaoDTO.setData(transacaoModel.getData());
         transacaoDTO.setValor(transacaoModel.getValor());
--- src/main/java/com/backend/itau/service/TransacaoService.java
+++ src/main/java/com/backend/itau/service/TransacaoService.java
@@ -1,5 +1,7 @@
 package com.backend.itau.service;
 
+import com.backend.itau.dto.TransacaoDTO;
+import com.backend.itau.mapper.TransacaoMapper;
 import com.backend.itau.model.TransacaoModel;
 import com.backend.itau.repository.TransacaoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,12 +11,14 @@ import org.springframework.stereotype.Service;
 public class TransacaoService {
 
     private final TransacaoRepository transacaoRepository;
+    private final TransacaoMapper transacaoMapper;
     @Autowired
-    public TransacaoService(TransacaoRepository transacaoRepository) {
+    public TransacaoService(TransacaoRepository transacaoRepository, TransacaoMapper transacaoMapper) {
         this.transacaoRepository = transacaoRepository;
+        this.transacaoMapper = transacaoMapper;
     }
 
-    public TransacaoModel realizaTransacao(TransacaoModel transacao) {
-        return transacaoRepository.save(transacao);
+    public TransacaoDTO realizaTransacao(TransacaoModel transacao) {
+        return transacaoMapper.toDTO(transacaoRepository.save(transacao));
     }
 }
