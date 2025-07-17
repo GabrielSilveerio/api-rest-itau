772dab7a7e905ec58f8b32d42982e5ae1043b96a
GabrielSilveerio <silverio.gabriel2004@gmail.com>
2025-07-17 10:50:18 -0300

    Transacao Controller

---
+++ src/main/java/com/backend/itau/controller/TransacaoController.java
@@ -0,0 +1,25 @@
+package com.backend.itau.controller;
+
+import com.backend.itau.dto.TransacaoDTO;
+import com.backend.itau.model.TransacaoModel;
+import com.backend.itau.repository.TransacaoRepository;
+import com.backend.itau.service.TransacaoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/transacao")
+@RestController
+public class TransacaoController {
+
+    private TransacaoService transacaoService;
+    @Autowired
+    public TransacaoController(TransacaoService transacaoService) {
+        this.transacaoService = transacaoService;
+    }
+    @PostMapping
+    public TransacaoDTO realizaTransacao(TransacaoModel transacao) {
+        return transacaoService.realizaTransacao(transacao);
+    }
+}
