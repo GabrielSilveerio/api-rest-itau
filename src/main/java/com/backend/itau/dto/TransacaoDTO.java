9236957ceaa65d2a5fb11d4382cff9f4b6306c36
GabrielSilveerio <silverio.gabriel2004@gmail.com>
2025-07-17 10:36:45 -0300

    Transacao DTO

---
+++ src/main/java/com/backend/itau/dto/TransacaoDTO.java
@@ -0,0 +1,24 @@
+package com.backend.itau.dto;
+
+import java.util.Date;
+
+public class TransacaoDTO {
+    private double valor;
+    private Date data;
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+}
--- src/main/java/com/backend/itau/model/TransacaoModel.java
+++ src/main/java/com/backend/itau/model/TransacaoModel.java
@@ -18,4 +18,27 @@ public class TransacaoModel {
     private double valor;
     private Date data;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
 }
